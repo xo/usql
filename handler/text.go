b62da0b3790151731d68d98dbb18311f40d1c218
Kenneth Shaw <kenneth.shaw@knq.io>
2017-03-16 20:54:48 +0700

    Updating text

--- handler/text.go
+++ handler/text.go
@@ -7,9 +7,11 @@ import (
 )
 
 const (
-	welcomeDesc = "Type \"help\" for help.\n\n"
-
 	notConnected = "(not connected)"
+
+	tag = `usql, the universal command-line interface for SQL databases.`
+
+	welcomeDesc = "Type \"help\" for help.\n\n"
 
 	queryBufferEmpty = "Query buffer is empty."
 
@@ -19,11 +21,11 @@ const (
 
 	extraArgumentIgnored = "\\%s: extra argument \"%s\" ignored\n"
 
-	copyright = "usql, the universal command-line interface for SQL databases.\n\n" + license + "\n"
+	copyright = tag + "\n\n" + license + "\n\n"
 
 	missingRequiredArg = "missing required argument"
 
-	helpDesc = `You are using usql, the universal command-line interface for SQL databases.
+	helpDesc = `You are using ` + tag + `
 Type: \copyright        distribution terms
       \c[onnect] <url>  connect to url
       \q                quit
@@ -31,7 +33,7 @@ Type: \copyright        distribution terms
 `
 )
 
-// cmdErr is a util func to simply write the a "\cmd: msg" style error.
+// cmdErr is a util func to simply write a "\cmd: msg" style error.
 func cmdErr(l *readline.Instance, cmd, msg string) (int, error) {
 	return fmt.Fprintf(l.Stderr(), "\\%s: %s\n", cmd, msg)
 }
