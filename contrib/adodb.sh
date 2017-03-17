df1e6fbda4e293d69777aaf64278810132192c3e
Kenneth Shaw <kenneth.shaw@knq.io>
2017-03-17 12:50:46 +0700

    Fixing typo in contrib/adodb.sh

--- contrib/adodb.sh
+++ contrib/adodb.sh
@@ -4,5 +4,5 @@ rm -f example.csv
 
 usql "adodb://Microsoft.ACE.OLEDB.12.0/?Extended+Properties=\"Text;HDR=NO;FMT=Delimited\"" \
   -c "create table example.csv(f1 text, f2 text, f3 text);" \
-  -c "insert into example.csv(f1, f2, f3), values ('a', 'b', 'c');" \
+  -c "insert into example.csv(f1, f2, f3) values ('a', 'b', 'c');" \
   -c "select * from example.csv;"
