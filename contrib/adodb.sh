ded2b0f878c6faeaff7fa493f24f9cc62f4f6a39
Kenneth Shaw <kenneth.shaw@knq.io>
2017-03-17 11:30:00 +0700

    Updates to contrib/adodb.sh for easier testing on Windows

--- contrib/adodb.sh
+++ contrib/adodb.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-usql 'adodb://Microsoft.ACE.OLEDB.12.0/?Extended+Properties="Text;HDR=NO;FMT=Delimited"'
+rm -f example.csv
+
+usql "adodb://Microsoft.ACE.OLEDB.12.0/?Extended+Properties=\"Text;HDR=NO;FMT=Delimited\"" \
+  -c "create table example.csv(f1 text, f2 text, f3 text);" \
+  -c "insert into example.csv(f1, f2, f3), values ('a', 'b', 'c');" \
+  -c "select * from example.csv;"
