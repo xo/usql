c4c9e344f6ba246b30341119f4f109347c938dc0
Kenneth Shaw <kenneth.shaw@knq.io>
2017-03-29 20:32:44 +0700

    Attempt to fix oracle build dependency for travis-ci

---
+++ contrib/grab-oracle.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+BASE=https://raw.githubusercontent.com/strongloop/loopback-oracle-builder/master/deps/oracle/Linux/x64
+
+if [ ! -f instantclient-basiclite-linux.x64-12.1.0.2.0.zip ]; then
+  wget $BASE/instantclient-basiclite-linux.x64-12.1.0.2.0.zip
+fi
+
+if [ ! -f instantclient-sdk-linux.x64-12.1.0.2.0.zip ]; then
+  wget $BASE/instantclient-sdk-linux.x64-12.1.0.2.0.zip
+fi
+
+rm -rf instantclient_12_1
+
+unzip instantclient-basiclite-linux.x64-12.1.0.2.0.zip
+unzip instantclient-sdk-linux.x64-12.1.0.2.0.zip
+
+DATA=$(cat << 'ENDSTR'
+prefix=./instantclient_12_1
+
+version=12.1
+build=client64
+
+libdir=${prefix}/lib
+includedir=${prefix}/sdk/include
+
+Name: OCI
+Description: Oracle database engine
+Version: ${version}
+Libs: -L${libdir} -lclntsh
+Libs.private:
+Cflags: -I${includedir}
+ENDSTR
+)
+
+echo "$DATA" > oci8.pc
