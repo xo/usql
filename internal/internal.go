9bc84a7cef141cfbef4eca420d589d1db7d83faf
Kenneth Shaw <kenneth.shaw@knq.io>
2017-10-10 01:08:31 +0700

    Removing avatica driver

--- internal/internal.go
+++ internal/internal.go
@@ -11,7 +11,6 @@ package internal
 func KnownBuildTags() map[string]string {
 	return map[string]string{
 		"adodb":      "adodb",       // github.com/mattn/go-adodb
-		"avatica":    "avatica",     // github.com/Boostport/avatica
 		"clickhouse": "clickhouse",  // github.com/kshvakov/clickhouse
 		"couchbase":  "n1ql",        // github.com/couchbase/go_n1ql
 		"firebird":   "firebirdsql", // github.com/nakagami/firebirdsql
