982e1e50e60806797faef7edfbaf189f067a88e6
Kenneth Shaw <kenneth.shaw@knq.io>
2018-06-16 12:56:37 +0700

    Standardizing contrib directory
    Cleaning up contrib directory, initial changes for supporting unit tests
    for multiple databases.
    
    Added contrib/docker-run.sh that will be used to consistently start a
    docker image for each database to test with.

---
+++ contrib/docker-run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DIR=$1
+
+SRC=$(realpath $(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))
+
+BASE=$SRC/$DIR
+
+if [ -z "$DIR" ]; then
+  echo "usage: $0 <NAME>"
+  exit 1
+fi
+
+if [ ! -e $BASE/docker-config ]; then
+  echo "error: $BASE/docker-config doesn't exist"
+  exit 1
+fi
+
+. $BASE/docker-config
+
+if [[ "$DIR" != "$NAME" ]]; then
+  echo "error: $BASE/docker-config is invalid"
+  exit 1
+fi
+
+echo "NAME: $NAME"
+
+shift
+
+# setup params
+declare -A PARAMS=()
+for k in NAME ENV PUBLISH VOLUME; do
+  n=$(tr 'A-Z' 'a-z' <<< "$k")
+  v=$(eval echo "\$$k")
+  if [ ! -z "$v" ]; then
+    PARAMS[$n]=$v
+  fi
+done
+
+docker stop $NAME
+
+docker rm $NAME
+
+set -e
+
+docker run \
+  --detach \
+  --rm \
+  $(for k in "${!PARAMS[@]}"; do echo --$k=${PARAMS[$k]}; done) \
+  $IMAGE
