a971ccf6c4aee9615041b2f4aa5f8cbd7c5f0bf8
Kenneth Shaw <kenneth.shaw@knq.io>
2018-09-23 04:41:07 +0700

    Adding docker-config for mysql + postgres and updating docker-run.sh

--- contrib/docker-run.sh
+++ contrib/docker-run.sh
@@ -16,16 +16,25 @@ if [ ! -e $BASE/docker-config ]; then
   exit 1
 fi
 
-. $BASE/docker-config
+source $BASE/docker-config
 
 if [[ "$DIR" != "$NAME" ]]; then
   echo "error: $BASE/docker-config is invalid"
   exit 1
 fi
 
-echo "NAME: $NAME"
+echo "NAME:    $NAME"
 
 shift
+
+UPDATE=0
+
+OPTIND=1
+while getopts "dfuv:" opt; do
+case "$opt" in
+  u) UPDATE=1 ;;
+esac
+done
 
 # setup params
 declare -a PARAMS
@@ -39,9 +48,27 @@ for k in NAME PUBLISH NETWORK VOLUME ENV; do
   fi
 done
 
-docker stop $NAME
+echo "IMAGE:   $IMAGE"
+echo "PUBLISH: $PUBLISH"
+echo "UPDATE:  $UPDATE"
 
-docker rm $NAME
+if [ "$UPDATE" -eq "1" ]; then
+  if [ ! -f $BASE/Dockerfile ]; then
+    docker pull $IMAGE
+  else
+    pushd $BASE &> /dev/null
+    docker build --pull -t $IMAGE:latest .
+    popd &> /dev/null
+  fi
+fi
+
+if [ ! -z "$(docker ps -q --filter "name=$NAME")" ]; then
+  docker stop $NAME
+fi
+
+if [ ! -z "$(docker ps -q -a --filter "name=$NAME")" ]; then
+  docker rm -f $NAME
+fi
 
 set -e
 
