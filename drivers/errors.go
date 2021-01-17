c326a327ff2ee00a953b5ff3023e27b6cc6fa8e8
Kenneth Shaw <kenshaw@gmail.com>
2021-01-17 19:47:20 +0700

    Minor fixup to last PR

--- drivers/errors.go
+++ drivers/errors.go
@@ -47,8 +47,10 @@ func (e *Error) Error() string {
 	return e.Driver + ": " + chop(e.Err.Error(), e.Driver)
 }
 
-// Unwrap returns the original error
-func (e *Error) Unwrap() error { return e.Err }
+// Unwrap returns the original error.
+func (e *Error) Unwrap() error {
+	return e.Err
+}
 
 // chop chops off a "prefix: " prefix from a string.
 func chop(s, prefix string) string {
