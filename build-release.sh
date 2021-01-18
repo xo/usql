f7ea3d545badbc6811f518a474fb58aba36cc240
Kenneth Shaw <kenshaw@gmail.com>
2021-01-19 05:46:35 +0700

    Updating build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -1,87 +1,73 @@
 #!/bin/bash
 
-VER=$1
-BUILD=$2
+set -e
 
-if [ -z "$VER" ]; then
-  echo "usage: $0 <VER>"
-  exit 1
-fi
-
-PLATFORM=$(uname|sed -e 's/_.*//'|tr '[:upper:]' '[:lower:]'|sed -e 's/^\(msys\|mingw\).*/windows/')
-TAG=$VER
-SRC=$(realpath $(cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd ))
-NAME=$(basename $SRC)
-EXT=tar.bz2
-
-if [ -z "$BUILD" ]; then
-  BUILD=$SRC/build
-fi
-
-DIR=$BUILD/$PLATFORM/$VER
-BIN=$DIR/$NAME
+SRC=$(realpath $(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd))
 
 TAGS="most sqlite_app_armor sqlite_fts5 sqlite_introspect sqlite_json1 sqlite_stat4 sqlite_userauth sqlite_vtable"
+VER=$(git tag -l v* $SRC|grep -E '^v[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$'|sort -r -V|head -1||:)
+BUILD=$SRC/build
 
+OPTIND=1
+while getopts "b:v:t:" opt; do
+case "$opt" in
+  b) BUILD=$OPTARG ;;
+  v) VER=$OPTARG ;;
+  t) TAGS=$OPTARG ;;
+esac
+done
+
+PLATFORM=$(uname|sed -e 's/_.*//'|tr '[:upper:]' '[:lower:]'|sed -e 's/^\(msys\|mingw\).*/windows/')
+ARCH=amd64
+NAME=$(basename $SRC)
+VER="${VER#v}"
+EXT=tar.bz2
+DIR=$BUILD/$PLATFORM/$VER
+BIN=$DIR/$NAME
 case $PLATFORM in
   windows)
     EXT=zip
     BIN=$BIN.exe
   ;;
-
   linux|darwin)
     TAGS="$TAGS sqlite_icu no_adodb"
   ;;
 esac
-
-OUT=$DIR/usql-$VER-$PLATFORM-amd64.$EXT
-
-echo "PLATFORM: $PLATFORM"
-echo "VER: $VER"
-echo "DIR: $DIR"
-echo "BIN: $BIN"
-echo "OUT: $OUT"
-echo "TAGS: $TAGS"
-
-set -e
-
-if [ -d $DIR ]; then
-  echo "removing $DIR"
-  rm -rf $DIR
-fi
-
-mkdir -p $DIR
+OUT=$DIR/$NAME-$VER-$PLATFORM-$ARCH.$EXT
 
 pushd $SRC &> /dev/null
-
+echo "APP:         $NAME/${VER} ($PLATFORM/$ARCH)"
+if [ ! -z "$TAGS" ]; then
+  echo "BUILD TAGS:  $TAGS"
+fi
+if [ -d $DIR ]; then
+  echo "REMOVING:    $DIR"
+  rm -rf $DIR
+fi
+mkdir -p $DIR
+echo "BUILDING:    $BIN"
 go build \
   -tags "$TAGS" \
   -ldflags="-s -w -X github.com/xo/usql/text.CommandName=$NAME -X github.com/xo/usql/text.CommandVersion=$VER" \
   -o $BIN
-
-echo -n "checking usql --version: "
-BUILT_VER=$($BIN --version)
-if [ "$BUILT_VER" != "usql $VER" ]; then
-  echo -e "\n\nerror: expected usql --version to report 'usql $VER', got: '$BUILT_VER'"
-  exit 1
-fi
-echo "$BUILT_VER"
-
 case $PLATFORM in
   linux|windows|darwin)
-    echo "stripping $BIN"
+    echo "STRIPPING:   $BIN"
     strip $BIN
   ;;
 esac
-
 case $PLATFORM in
   linux|windows|darwin)
-    echo "packing $BIN"
-    upx -q -q $BIN
+    COMPRESSED=$(upx -q -q $BIN|awk '{print $1 " -> " $3 " (" $4 ")"}')
+    echo "COMPRESSED:  $COMPRESSED"
   ;;
 esac
-
-echo "compressing $OUT"
+BUILT_VER=$($BIN --version)
+if [ "$BUILT_VER" != "$NAME ${VER#v}" ]; then
+  echo -e "\n\nerror: expected $NAME --version to report '$NAME ${VER#v}', got: '$BUILT_VER'"
+  exit 1
+fi
+echo "REPORTED:    $BUILT_VER"
 case $EXT in
   tar.bz2)
     tar -C $DIR -cjf $OUT $(basename $BIN)
@@ -90,7 +76,5 @@ case $EXT in
     zip $OUT -j $BIN
   ;;
 esac
-
-du -sh $OUT
-
+echo "PACKED:      $OUT ($(du -sh $OUT|awk '{print $1}'))"
 popd &> /dev/null
