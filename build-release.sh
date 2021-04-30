f21e52ad4cfa2fbd4de04ae5832ab500886ac636
Kenneth Shaw <kenshaw@gmail.com>
2021-05-01 06:12:45 +0700

    Updates build-release.sh and most.sh to produce fully static builds
    Changes build-release.sh and most.sh to produce binaries statically
    linked against ICU libraries (needed for advanced sqlite3 features).
    
    All attribution/props to Jan Was (@nineinchnick).

--- build-release.sh
+++ build-release.sh
@@ -2,10 +2,24 @@
 
 set -e
 
-TAGS="most sqlite_app_armor sqlite_fts5 sqlite_introspect sqlite_json1 sqlite_stat4 sqlite_userauth sqlite_vtable"
-
 SRC=$(realpath $(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd))
 
+TAGS=(
+  most
+  sqlite_icu
+  sqlite_app_armor
+  sqlite_fts5
+  sqlite_introspect
+  sqlite_json1
+  sqlite_stat4
+  sqlite_userauth
+  sqlite_vtable
+  osusergo
+  netgo
+  static_build
+)
+
+# get latest tag version
 pushd $SRC &> /dev/null
 VER=$(git tag -l|grep -E '^v[0-9]+\.[0-9]+\.[0-9]+(\.[0-9]+)?$'|sort -r -V|head -1||:)
 popd &> /dev/null
@@ -13,11 +27,10 @@ popd &> /dev/null
 BUILD=$SRC/build
 
 OPTIND=1
-while getopts "b:v:t:" opt; do
+while getopts "b:v:" opt; do
 case "$opt" in
   b) BUILD=$OPTARG ;;
   v) VER=$OPTARG ;;
-  t) TAGS=$OPTARG ;;
 esac
 done
 
@@ -34,38 +47,76 @@ case $PLATFORM in
     BIN=$BIN.exe
   ;;
   linux|darwin)
-    TAGS="$TAGS sqlite_icu no_adodb"
+    TAGS+=(no_adodb)
   ;;
 esac
 OUT=$DIR/$NAME-$VER-$PLATFORM-$ARCH.$EXT
 
 pushd $SRC &> /dev/null
 echo "APP:         $NAME/${VER} ($PLATFORM/$ARCH)"
-if [ ! -z "$TAGS" ]; then
-  echo "BUILD TAGS:  $TAGS"
-fi
+echo "BUILD TAGS:  ${TAGS[@]}"
 if [ -d $DIR ]; then
   echo "REMOVING:    $DIR"
   rm -rf $DIR
 fi
 mkdir -p $DIR
 echo "BUILDING:    $BIN"
-go build \
-  -tags "$TAGS" \
-  -ldflags="-s -w -X github.com/xo/usql/text.CommandName=$NAME -X github.com/xo/usql/text.CommandVersion=$VER" \
-  -o $BIN
+
+# build parameters
+TAGS="${TAGS[@]}"
+EXTLDFLAGS=(
+  -fno-PIC
+  -static
+  -licuuc
+  -licui18n
+  -licudata
+  -ldl
+)
+EXTLDFLAGS="${EXTLDFLAGS[@]}"
+LDFLAGS=(
+  -s
+  -w
+  -X github.com/xo/usql/text.CommandName=$NAME
+  -X github.com/xo/usql/text.CommandVersion=$VER
+  -linkmode=external
+  -extldflags \'$EXTLDFLAGS\'
+  -extld g++
+)
+LDFLAGS="${LDFLAGS[@]}"
+
+log() {
+  cat - | while read -r message; do
+    echo "$1$message"
+  done
+}
+
+# build
+(set -x;
+  go build \
+    -tags="$TAGS" \
+    -gccgoflags="all=-DU_STATIC_IMPLEMENTATION" \
+    -buildmode=pie \
+    -ldflags="$LDFLAGS" \
+    -o $BIN
+) 2>&1 | log 'BUILDING:    '
+
+# strip
 case $PLATFORM in
   linux|windows|darwin)
     echo "STRIPPING:   $BIN"
     strip $BIN
   ;;
 esac
+
+# compress
 case $PLATFORM in
   linux|windows|darwin)
     COMPRESSED=$(upx -q -q $BIN|awk '{print $1 " -> " $3 " (" $4 ")"}')
     echo "COMPRESSED:  $COMPRESSED"
   ;;
 esac
+
+# check build
 BUILT_VER=$($BIN --version)
 if [ "$BUILT_VER" != "$NAME ${VER#v}" ]; then
   echo -e "\n\nerror: expected $NAME --version to report '$NAME ${VER#v}', got: '$BUILT_VER'"
@@ -82,4 +133,3 @@ case $EXT in
 esac
 echo "PACKED:      $OUT ($(du -sh $OUT|awk '{print $1}'))"
 popd &> /dev/null
-
--- most.sh
+++ most.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
 
-TAGS="most sqlite_app_armor sqlite_fts5 sqlite_icu sqlite_introspect sqlite_json1 sqlite_stat4 sqlite_userauth sqlite_vtable"
+NAME=usql
+VER="$(date +%y.%m.%d)-dev"
 
-go build -tags "$TAGS" $@
+TAGS=(
+  most
+  sqlite_icu
+  sqlite_app_armor
+  sqlite_fts5
+  sqlite_introspect
+  sqlite_json1
+  sqlite_stat4
+  sqlite_userauth
+  sqlite_vtable
+  osusergo
+  netgo
+  static_build
+)
+TAGS="${TAGS[@]}"
+
+EXTLDFLAGS=(
+  -fno-PIC
+  -static
+  -licuuc
+  -licui18n
+  -licudata
+  -ldl
+)
+EXTLDFLAGS="${EXTLDFLAGS[@]}"
+
+LDFLAGS=(
+  -s
+  -w
+  -X github.com/xo/usql/text.CommandName=$NAME
+  -X github.com/xo/usql/text.CommandVersion=$VER
+  -linkmode=external
+  -extldflags \'$EXTLDFLAGS\'
+  -extld g++
+)
+LDFLAGS="${LDFLAGS[@]}"
+
+(set -x;
+  go build \
+    -tags="$TAGS" \
+    -gccgoflags="all=-DU_STATIC_IMPLEMENTATION" \
+    -buildmode=pie \
+    -ldflags="$LDFLAGS" \
+    $@
+)
