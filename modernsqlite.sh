70f37185b5b56513203bb697a70e190b44e9203f
Kenneth Shaw <kenshaw@gmail.com>
2021-05-30 05:03:55 +0700

    Adding options to most.sh build script, and remove moderncsqlite.sh script
    Adds command-line flags to most.sh to disable sqlite3 and CGO (in turn
    causing moderncsqlite to register aliases for sqlite3). Makes
    moderncsqlite.sh script redundant.

--- modernsqlite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-TAGS="most no_sqlite3 no_adodb moderncsqlite"
-
-CGO_ENABLED=0 go build -tags "$TAGS" $@
--- most.sh
+++ most.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# most.sh builds a development version of usql with "most" drivers.
+#
+# Options:
+#  -m   disable sqlite3 driver (no_sqlite3) and diasble CGO.
+#       causes moderncsqlite to register aliases for sqlite3.
+#  -v   toggle go build -v
+#  -x   toggle go build -x
+
 NAME=usql
 VER="$(date +%y.%m.%d)-dev"
 
 PLATFORM=$(uname|sed -e 's/_.*//'|tr '[:upper:]' '[:lower:]'|sed -e 's/^\(msys\|mingw\).*/windows/')
 
+CGO_ENABLED=1
 TAGS=(
   most
+)
+SQLITE_TAGS=(
   sqlite_app_armor
   sqlite_fts5
   sqlite_introspect
@@ -15,14 +26,28 @@ TAGS=(
   sqlite_userauth
   sqlite_vtable
 )
+EXTRA=()
 
 case $PLATFORM in
   darwin|linux)
-    TAGS+=(sqlite_icu no_adodb)
+    TAGS+=(no_adodb)
+    SQLITE_TAGS+=(sqlite_icu)
   ;;
 esac
 
-TAGS="${TAGS[@]}"
+OPTIND=1
+while getopts "mvx" opt; do
+case "$opt" in
+  m)
+    SQLITE_TAGS=(no_sqlite3)
+    CGO_ENABLED=0
+    ;;
+  v) EXTRA+=(-v) ;;
+  x) EXTRA+=(-x) ;;
+esac
+done
+
+TAGS="${TAGS[@]} ${SQLITE_TAGS[@]}"
 LDFLAGS=(
   -s
   -w
@@ -32,8 +57,8 @@ LDFLAGS=(
 LDFLAGS="${LDFLAGS[@]}"
 
 (set -x;
-  go build \
+  CGO_ENABLED=$CGO_ENABLED go build \
     -tags="$TAGS" \
     -ldflags="$LDFLAGS" \
-    $@
+    ${EXTRA[@]}
 )
