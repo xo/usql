44a8086a66be20001e8727448ea2833040f7d60f
Kenneth Shaw <ken@usql.app>
2021-06-21 07:01:15 +0700

    Modifying docker-run.sh to remove dangling images

--- contrib/docker-run.sh
+++ contrib/docker-run.sh
@@ -41,37 +41,39 @@ docker_run() {
     echo "error: $BASE/docker-config doesn't exist"
     exit 1
   fi
+  # load parameters from docer-config
   unset IMAGE NAME PUBLISH ENV VOLUME NETWORK PRIVILEGED PARAMS
   source $BASE/docker-config
   if [[ "$TARGET" != "$NAME" ]]; then
     echo "error: $BASE/docker-config is invalid"
     exit 1
   fi
-  echo "-------------------------------------------"
-  echo "NAME:       $NAME"
   # setup params
-  declare -a PARAMS
+  PARAMS=()
   for k in NAME PUBLISH ENV VOLUME NETWORK PRIVILEGED; do
     n=$(tr 'A-Z' 'a-z' <<< "$k")
     v=$(eval echo "\$$k")
     if [ ! -z "$v" ]; then
       for p in $v; do
-        PARAMS=("${PARAMS[@]}" "--$n=$p")
+        PARAMS+=("--$n=$p")
       done
     fi
   done
-
+  # determine if image exists
   EXISTS=$(docker image ls -q $IMAGE)
   if [[ "$UPDATE" == "0" && -z "$EXISTS" ]]; then
     UPDATE=1
   fi
+  # show parameters
+  echo "-------------------------------------------"
+  echo "NAME:       $NAME"
   echo "IMAGE:      $IMAGE (update: $UPDATE)"
   echo "PUBLISH:    $PUBLISH"
   echo "ENV:        $ENV"
   echo "VOLUME:     $VOLUME"
   echo "NETWORK:    $NETWORK"
   echo "PRIVILEGED: $PRIVILEGED"
-
+  # update
   if [[ "$UPDATE" == "1" && "$TARGET" != "oracle" ]]; then
     if [ ! -f $BASE/Dockerfile ]; then
       (set -ex;
@@ -84,12 +86,21 @@ docker_run() {
       )
       popd &> /dev/null
     fi
+    REF=$(awk -F: '{print $1}' <<< "$IMAGE")
+    REMOVE=$(docker image list --filter=dangling=true --filter=reference=$IMAGE -q)
+    if [ ! -z "$REMOVE" ]; then
+      (set -ex;
+        docker image rm -f $REMOVE
+      )
+    fi
   fi
+  # stop any running images
   if [ ! -z "$(docker ps -q --filter "name=$NAME")" ]; then
     (set -x;
       docker stop $NAME
     )
   fi
+
   if [ ! -z "$(docker ps -q -a --filter "name=$NAME")" ]; then
     (set -x;
       docker rm -f $NAME
