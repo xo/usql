e51fed1fd1f596aa3c7a196c16a3aa30d5029321
Kenneth Shaw <ken@usql.app>
2021-07-07 06:41:06 +0700

    Minor changes to most.sh

--- most.sh
+++ most.sh
@@ -3,10 +3,14 @@
 # most.sh builds a development version of usql with "most" drivers.
 #
 # Options:
-#  -m   disable sqlite3 driver (no_sqlite3) and diasble CGO.
-#       causes moderncsqlite to register aliases for sqlite3.
-#  -v   toggle go build -v
-#  -x   toggle go build -x
+#  -i        go install instead of go build
+#  -m        disable sqlite3 driver (no_sqlite3) and diasble CGO.
+#            causes moderncsqlite to register aliases for sqlite3.
+#  -v        add -v to go build/install
+#  -x        add -x to go build/install
+#  -t <TAG>  add build tag
+#
+# Usage: ./most.sh -i -t godror -v
 
 NAME=usql
 VER="$(date +%y.%m.%d)-dev"
@@ -15,9 +19,7 @@ PLATFORM=$(uname|sed -e 's/_.*//'|tr '[:upper:]' '[:lower:]'|sed -e 's/^\(msys\|
 
 CGO_ENABLED=1
 BUILDVERB=build
-TAGS=(
-  most
-)
+TAGS=()
 SQLITE_TAGS=(
   sqlite_app_armor
   sqlite_fts5
@@ -40,17 +42,14 @@ OPTIND=1
 while getopts "imvxt:" opt; do
 case "$opt" in
   i) BUILDVERB=install ;;
-  m)
-    CGO_ENABLED=0
-    SQLITE_TAGS=(no_sqlite3)
-    ;;
+  m) CGO_ENABLED=0 SQLITE_TAGS=(no_sqlite3) ;;
   v) EXTRA+=(-v) ;;
   x) EXTRA+=(-x) ;;
   t) TAGS+=($OPTARG) ;;
 esac
 done
 
-TAGS="${TAGS[@]} ${SQLITE_TAGS[@]}"
+TAGS="most ${SQLITE_TAGS[@]} ${TAGS[@]}"
 LDFLAGS=(
   -s
   -w
