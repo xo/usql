7b4a1b031d2b51e0c5d9b2a011bb261309890319
Sven Rebhan <srebhan@influxdata.com>
2023-04-13 23:18:34 +0200

    Avoid custom version check and disable multi-line comments

--- drivers/flightsql/flightsql.go
+++ drivers/flightsql/flightsql.go
@@ -4,25 +4,10 @@
 package flightsql
 
 import (
-	"context"
-
 	_ "github.com/apache/arrow/go/v12/arrow/flight/flightsql/driver" // DRIVER
 	"github.com/xo/usql/drivers"
 )
 
 func init() {
-	drivers.Register("flightsql", drivers.Driver{
-		AllowMultilineComments: true,
-		Version: func(ctx context.Context, db drivers.DB) (string, error) {
-			var ver string
-			err := db.QueryRowContext(
-				ctx,
-				`SELECT version()`,
-			).Scan(&ver)
-			if err != nil {
-				return "", err
-			}
-			return "FlightSQL " + ver, nil
-		},
-	})
+	drivers.Register("flightsql", drivers.Driver{})
 }
