3909aab36ec50402ceb494a9bc324e062aee3a39
karol.dziki <karol.dziki@datawalk.com>
2023-06-05 16:59:04 +0200

    Add support for custom tls config in Vertica driver

--- drivers/vertica/vertica.go
+++ drivers/vertica/vertica.go
@@ -5,16 +5,65 @@ package vertica
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"database/sql"
+	"fmt"
+	"io"
+	"net/url"
 	"os"
 	"regexp"
 	"strings"
 
-	_ "github.com/vertica/vertica-sql-go" // DRIVER
+	vertigo "github.com/vertica/vertica-sql-go" // DRIVER
 	"github.com/vertica/vertica-sql-go/logger"
+	"github.com/xo/dburl"
 	"github.com/xo/usql/drivers"
 )
 
 func init() {
+	// List of custom TLS configurations that may be applied via query in connection string.
+	customTlsConfig := map[string]func(string, *tls.Config) error{
+		"ca_path": func(queryValue string, c *tls.Config) error {
+			rootCertPool := x509.NewCertPool()
+
+			pem, err := os.ReadFile(queryValue)
+			if err != nil {
+				return err
+			}
+
+			if ok := rootCertPool.AppendCertsFromPEM(pem); !ok {
+				return fmt.Errorf("error: failed to append pem to cert pool")
+			}
+
+			c.RootCAs = rootCertPool
+
+			return nil
+		},
+	}
+
+	hasCustomTlsConfig := func(queries url.Values) bool {
+		for key := range customTlsConfig {
+			if queries.Has(key) {
+				return true
+			}
+		}
+
+		return false
+	}
+
+	applyCustomTlsConfig := func(queries url.Values, tlsConfig *tls.Config) error {
+		for key, configFunction := range customTlsConfig {
+			if queries.Has(key) {
+				if err := configFunction(queries.Get(key), tlsConfig); err != nil {
+					return err
+				}
+			}
+		}
+
+		return nil
+	}
+
 	// turn off logging
 	if os.Getenv("VERTICA_SQL_GO_LOG_LEVEL") == "" {
 		logger.SetLogLevel(logger.NONE)
@@ -29,6 +78,45 @@ func init() {
 				return "", err
 			}
 			return ver, nil
+		},
+		Open: func(_ context.Context, u *dburl.URL, stdout, stderr func() io.Writer) (func(string, string) (*sql.DB, error), error) {
+			return func(_, _ string) (*sql.DB, error) {
+				queries := u.Query()
+
+				if hasCustomTlsConfig(queries) {
+					if queries.Get("tlsmode") != "server-strict" {
+						configNames := []string{}
+
+						for key := range customTlsConfig {
+							configNames = append(configNames, key)
+						}
+
+						return nil, fmt.Errorf(fmt.Sprintf("error: when custom tls configurations are set (%s), tlsmode must be set to server-strict", strings.Join(configNames, ",")))
+					}
+
+					tlsConfig := &tls.Config{ServerName: u.Hostname()}
+
+					if err := applyCustomTlsConfig(queries, tlsConfig); err != nil {
+						return nil, err
+					}
+
+					if err := vertigo.RegisterTLSConfig("custom_tls_config", tlsConfig); err != nil {
+						return nil, err
+					}
+
+					queries.Set("tlsmode", "custom_tls_config")
+				}
+
+				dsn := url.URL{
+					Scheme:   u.Driver,
+					User:     u.User,
+					Host:     u.Host,
+					Path:     u.Path,
+					RawQuery: queries.Encode(),
+				}
+
+				return sql.Open(u.Driver, dsn.String())
+			}, nil
 		},
 		ChangePassword: func(db drivers.DB, user, newpw, _ string) error {
 			_, err := db.Exec(`ALTER USER ` + user + ` IDENTIFIED BY '` + newpw + `'`)
